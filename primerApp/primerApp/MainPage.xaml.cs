2d5eaf3bf2029b887b097df69377b10c7c5332b7
juanesherrera21 <juanesherrera21@hotmail.com>
2022-09-09 19:05:23 -0500

    Agregar archivos de proyecto.

---
+++ primerApp/primerApp/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace primerApp
+{
+    public partial class Page1 : ContentPage
+
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+    }
+}
